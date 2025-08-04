d1ceff8e3f269ea56b718d6050616c46f86423f9
bhrugen <bhrugen1990@gmail.com>
2025-08-04 08:37:08 -0500

    Section 16 - Cart slice and local storage

---
+++ candleopedia/src/store/slice/cartSlice.js
@@ -0,0 +1,39 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const saveToLocalStorage = (state) => {
+  try {
+    localStorage.setItem("cart_CandleOPedia", JSON.stringify(state));
+  } catch (error) {
+    console.error("Error saving cart to localStorage:", error);
+  }
+};
+
+const initialState = {
+  items: [],
+  totalQuantity: 0,
+  totalAmount: 0,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const { product, quantity = 1 } = action.payload;
+      const existingItem = state.items.find((item) => item.id === product.id);
+
+      if (existingItem) {
+        existingItem.quantity += quantity;
+      } else {
+        state.items.push({
+          ...product,
+          quantity,
+        });
+      }
+
+      state.totalQuantity += quantity;
+      state.totalAmount += product.price * quantity;
+      saveToLocalStorage(state);
+    },
+  },
+});

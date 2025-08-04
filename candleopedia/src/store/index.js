b1f6891d02b05ea09dc9b6565313e10bc294af4f
bhrugen <bhrugen1990@gmail.com>
2025-08-04 11:57:26 -0500

    Section 16 - Load state from local storage

--- candleopedia/src/store/index.js
+++ candleopedia/src/store/index.js
@@ -1,10 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { baseApi } from "./api/baseApi";
 import cartReducer from "./slice/cartSlice";
+
+const loadCartFromStorage = () => {
+  try {
+    const savedCart = localStorage.getItem("cart_CandleOPedia");
+    return savedCart ? JSON.parse(savedCart) : undefined;
+  } catch (error) {
+    console.error("Error saving cart to localStorage:", error);
+  }
+};
+
 export const store = configureStore({
   reducer: {
     [baseApi.reducerPath]: baseApi.reducer,
     cart: cartReducer,
+  },
+  preloadedState: {
+    cart: loadCartFromStorage(),
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(baseApi.middleware),
